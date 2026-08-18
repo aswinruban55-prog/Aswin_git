1caab6175fedf57bb694e08f758e014a7d166502
aswinruban55-prog <aswinruban55@gmail.com>
2026-08-18 12:29:11 +0530

    main2.py file created

---
+++ main2.py
@@ -0,0 +1,13 @@
+class Employee:
+
+    def name(self):
+        print('my name is aswin kumar')
+
+class work(Employee):
+
+    def work(self):
+        print('data engineer')
+
+w = work()
+w.name()
+w.work()
